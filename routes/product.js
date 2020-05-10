880513bc654b48ccd7b2f24c0975eb20958d5079
Aniket Ghosh <ghoshaniketkumar7@gmail.com>
2020-05-11 00:55:53 +0530

    all done, problem with one product route

--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,26 @@ const router = express.Router();
 
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
-const { getProductById, createProduct } = require("../controllers/product");
+const { getProductById, createProduct, getProduct, getAllUniqueCategories, getAllProducts, photo, removeProduct, updateProduct } = require("../controllers/product");
 
 // all parameters
-router.param("userId",getUserById);
-router.param("productId",getProductById);
+router.param("userId", getUserById);
+router.param("productId", getProductById);
 
 
 //actual routes
-router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct);
+//auth routes
+router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
+// read routes
+router.get("/product/:productId", getProduct);
+router.get("/product/photo/:productId", photo); // performance optimization
+
+//delete route
+router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, removeProduct);
+//update route
+router.put("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, updateProduct);
+
+//listing route
+router.get("/product", getAllProducts);
+router.get("/products/categories", getAllUniqueCategories);
 module.exports = router;
